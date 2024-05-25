2ee099240b174903c9cc3be34245808d788c5b31
FedericoFrau <fedeafrau@gmail.com>
2024-05-24 22:56:49 -0300

    welcomeFrame and styles added

--- memoryGame/js/components/WelcomeFrame.js
+++ memoryGame/js/components/WelcomeFrame.js
@@ -1,12 +1,11 @@
-// import '../../styles/welcomeFrame.css'
-
 const WelcomeFrame = () => {
-
-  return `
-  <div class="welcomeContainer">
-  <h2 class="welcomeText">Welcome! Please select a game to start having fun...</h2>
-  </div>
-  `
+    const main = document.querySelector("main");
+    const HTMLContent = `
+        <div class="welcomeContainer">
+            <h2 class="welcomeText">Welcome!</h2>
+            <h2 class="welcomeText">Please select a game to start having fun...</h2>
+        </div>`
+        main.innerHTML = HTMLContent
 }
 
-export default WelcomeFrame
+export default WelcomeFrame;
